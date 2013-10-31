e174c9c652aed5c1e68caf52372f90bbdf03ee6e
Mu-An Chiou <me@muanchiou.com>
2013-10-31 11:53:32 +0000

    Add command line test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,17 @@
 var test = require("tape")
 var spiffing = require("../")
+var child = require("child_process")
 
 test("has correct spelling", function(t) {
   t.equal( spiffingDoge(), ".doge { color: #000 }" )
   t.end()
+})
+
+test("command line works", function(t) {
+  child.exec("spiffing", function(error, stdout, stderr) {
+    t.equal( stdout, "Converted target to target-converted.css!\n" )
+    t.end()
+  })
 })
 
 function spiffingDoge() {
