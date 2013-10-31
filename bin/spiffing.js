7d4d018dd54cbdafd8a12d39422a10a261cd7c19
Mu-An Chiou <me@muanchiou.com>
2013-10-31 01:19:35 +0000

    Killing spaces

--- bin/spiffing.js
+++ bin/spiffing.js
@@ -9,25 +9,25 @@ var input = process.argv[2] || "."
 if (isDir(input)) convertCssFilesInCurrentDir(input)
 else convertFile(input)
 
-function isDir (input) {
+function isDir(input) {
   return input ? fs.lstatSync(input).isDirectory() : true
 }
 
-function convertCssFilesInCurrentDir (dir) {
+function convertCssFilesInCurrentDir(dir) {
   var glob = require('glob')
   var files = []
 
   glob(dir + "/*.css", findAndProcessFiles )
 }
 
-function findAndProcessFiles (err, results) {
+function findAndProcessFiles(err, results) {
   results.filter(function(result){
     return !result.match("-converted.css")
   }).map(convertFile)
 }
 
-function convertFile (file) {
-  fs.readFile( file, "utf8" , function (errRead, data) {
+function convertFile(file) {
+  fs.readFile( file, "utf8" , function(errRead, data) {
     if (errRead) throw errRead
 
     filename = path.basename(file, '.css')
